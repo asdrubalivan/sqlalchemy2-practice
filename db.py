353914b1658f2b072b81e46773202eb7634990ff
Asdrúbal Iván Suárez Rivera <asdrubal.ivan.suarez.rivera@gmail.com>
2023-02-04 09:33:31 -0400

    Rest of commands

--- db.py
+++ db.py
@@ -2,6 +2,9 @@ from sqlalchemy import create_engine, Engine
 from typing import Callable, Optional
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, sessionmaker, Session
 from sqlalchemy.types import Boolean, String
+from rich.repr import RichReprResult
+from rich.console import RenderResult, Console, ConsoleOptions
+from rich.table import Table
 
 __all__ = ["get_engine", "Session", "TodoItem"]
 
@@ -25,6 +28,24 @@ class TodoItem(Base):
     finished: Mapped[bool] = mapped_column(Boolean())
     name: Mapped[str] = mapped_column(String())
     description: Mapped[Optional[str]]
+
+    def __rich_console__(
+        self, console: Console, options: ConsoleOptions
+    ) -> RenderResult:
+        yield f"[b]Todo #{self.id}[/b]"
+        table = Table("Attribute", "Value")
+        table.add_row("id", str(self.id))
+        table.add_row("finished", "[green]yes" if self.finished else "[red]no")
+        table.add_row("name", self.name)
+        table.add_row("description", self.description)
+        yield table
+
+    def __rich_repr__(self) -> RichReprResult:
+        yield "ID", self.id
+        yield "Finished", ("yes" if self.finished else "no")
+        yield "Name", self.name
+        if self.description is not None:
+            yield "Description", self.description
 
 
 def create_tables() -> None:
--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import typer
+import click
+from rich.panel import Panel
+from rich.pretty import Pretty
 import db
 from db import TodoItem
+from rich import print
 
 app = typer.Typer()
 
@@ -8,13 +12,38 @@ app = typer.Typer()
 @app.command()
 def create_todo(name: str, finished: bool = True) -> None:
     with db.session() as session:
-        session.add(TodoItem(finished=finished))
+        session.add(TodoItem(name=name, finished=finished))
         session.commit()
 
 
 @app.command()
+def list_todos() -> None:
+    with db.session() as session:
+        values = session.query(TodoItem).all()
+    for v in values:
+        print(Panel(v))
+
+
+@app.command()
+def get_todo_by_id(id: int) -> None:
+    with db.session() as session:
+        print(session.query(TodoItem).get(id))
+
+
+@app.command()
+def mark_todo(id: int, finished: bool) -> None:
+    with db.session() as session:
+        value = session.query(TodoItem).get(id)
+        if value:
+            value.finished = finished
+        session.add(value)
+        session.commit()
+        print("Value updated", value)
+
+
+@app.command()
 def create_tables() -> None:
-    print("Creating tables")
+    print("[green] Creating tables")
     db.create_tables()
 
 
